4069453283d2f765bbb0c19a8aee686a93649dd1
DEWEY KWAK <dewey@DEWEYui-noteubug.local>
2024-10-02 11:23:00 +0900

    Automated commit on 2024010-02 11:23:00

--- test/1002.py
+++ test/1002.py
@@ -14,5 +14,7 @@ print(df)
 
 # ascending = True (기본값) : 오름차순
 # ascending = False : 내림차순
-print(df.sort_values(by='현재가',ascending=False)) # 내림차순
-print(df.sort_values(by='현재가',ascending=True))
+print(df.sort_values(by='종목명',ascending=False)) # 내림차순
+print(df.sort_values(by='종목명',ascending=True)) # 오름차순
+
+
