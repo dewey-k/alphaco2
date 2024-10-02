6f1df4e4a4e55906d69923a84d58e8cf38e142b0
DEWEY KWAK <dewey@DEWEYui-noteubug.local>
2024-10-02 11:22:00 +0900

    Automated commit on 2024010-02 11:22:00

--- test/1002.py
+++ test/1002.py
@@ -12,6 +12,7 @@ df = DataFrame(data=data, columns=columns)
 df.set_index("종목코드", inplace=True)
 print(df)
 
-
+# ascending = True (기본값) : 오름차순
+# ascending = False : 내림차순
 print(df.sort_values(by='현재가',ascending=False)) # 내림차순
-
+print(df.sort_values(by='현재가',ascending=True))
