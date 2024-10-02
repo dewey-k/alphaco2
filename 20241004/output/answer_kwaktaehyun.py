5b1363c3aba1068d24cf87abbdd475cfbf0ef834
DEWEY KWAK <dewey@DEWEYui-noteubug.local>
2024-10-02 10:21:00 +0900

    Automated commit on 2024010-02 10:21:00

--- 20241004/output/answer_kwaktaehyun.py
+++ 20241004/output/answer_kwaktaehyun.py
@@ -9,5 +9,13 @@ import seaborn
 개발환경설정 및 깃허브 push 전 과정 서술
 virtualenv venv > source venv/bin/activate
 git add .
-git commit -m "
+git commit -m "message"
+git push
 """
+
+# 문제2
+# 시각화 이미지 내보내기
+# 데이터 프레임 내보내기
+
+text = [1,2,3]
+print(text)
