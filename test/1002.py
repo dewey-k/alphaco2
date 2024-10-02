a949a7802862a354f1b197feb0bd8a83d0bd45a9
DEWEY KWAK <dewey@DEWEYui-noteubug.local>
2024-10-02 11:26:00 +0900

    Automated commit on 2024010-02 11:26:00

--- test/1002.py
+++ test/1002.py
@@ -10,12 +10,13 @@ data = [
 columns = ["종목코드", "종목명", "현재가"]
 df = DataFrame(data=data, columns=columns)
 df.set_index("종목코드", inplace=True)
-print(df)
+# print(df)
 
 # ascending = True (기본값) : 오름차순
 # ascending = False : 내림차순
-print(df.sort_values(by='종목명',ascending=False)) # 내림차순
-print(df.sort_values(by='종목명',ascending=True)) # 오름차순
+# print(df.sort_values(by='종목명',ascending=False)) # 내림차순
+# print(df.sort_values(by='종목명',ascending=True)) # 오름차순
 
-print(df.sort_index()) # 기본값
-print(df.sort_index(ascending=False)) # 역순
+# print(df.sort_index()) # 기본값
+# print(df.sort_index(ascending=False)) # 역순
+
