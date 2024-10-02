e699d071aedb87db57be2f0cad70d1676753696a
DEWEY KWAK <dewey@DEWEYui-noteubug.local>
2024-10-02 11:47:00 +0900

    Automated commit on 2024010-02 11:47:00

--- test/1002.py
+++ test/1002.py
@@ -51,8 +51,8 @@ print(df)
 
 # DataFrameGroubBy 타입으로 변환했다가, DataFrame 타입으로 다시 변환
 result = df.groupby('테마')[['PER','PBR']].mean()
-print(result, type(result))
+# print(result, type(result))
 
 print(df.groupby('테마').get_group('2차전지(생산)'))
-print(df.groupby('테마').get_group('2차전지(생산)'))
-print(df.groupby('테마').get_group('2차전지(생산)'))
+print(df.groupby('테마').get_group('시스템반도체'))
+print(df.groupby('테마').get_group('해운'))
